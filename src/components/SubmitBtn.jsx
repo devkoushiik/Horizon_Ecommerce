0091134c6ed102abc3e2fdeecbe20915a5d483f0
Koushik Ahmed <koushikxahmed@gmail.com>
2023-12-01 18:41:01 +0600

    spinner added in submit button done

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,7 +1,20 @@
+import { useNavigation } from "react-router-dom";
 const SubmitBtn = ({ text }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
-    <button type="submit" className="btn btn-primary btn-block">
-      {text}
+    <button
+      disabled={isSubmitting}
+      type="submit"
+      className="btn btn-primary btn-block"
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span>sending...
+        </>
+      ) : (
+        text || "submit"
+      )}
     </button>
   );
 };
