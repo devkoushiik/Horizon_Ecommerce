71b926efad3f648b19c68ae499883ad1b60ea791
Koushik Ahmed <koushikxahmed@gmail.com>
2023-12-10 16:18:32 +0600

    single product page done.

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -7,11 +7,21 @@ export const customFetch = axios.create({
 });
 
 // converting to dollar
-
 export const formatPrice = (price) => {
   const dollarAmount = new Intl.NumberFormat("en-us", {
     style: "currency",
     currency: "USD",
   }).format((price / 100).toFixed(2));
   return dollarAmount;
+};
+// generate dynamic select option
+export const generateAmountOptions = (number) => {
+  return Array.from({ length: number }, (_, index) => {
+    const amount = index + 1;
+    return (
+      <option key={amount} value={amount}>
+        {amount}
+      </option>
+    );
+  });
 };
