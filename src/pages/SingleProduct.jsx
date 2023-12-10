5b54472e92935ba76c452723978cff7ea2b6ea2e
Koushik Ahmed <koushikxahmed@gmail.com>
2023-12-10 15:36:03 +0600

    single product access done.

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,20 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { formatPrice, customFetch } from "../utils";
+import { useState } from "react";
+
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+
+  return { product: response.data.data };
+};
+
 const SingleProduct = () => {
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarAmount = formatPrice(price);
+
+  console.log(product);
   return <div>SingleProduct</div>;
 };
 export default SingleProduct;
