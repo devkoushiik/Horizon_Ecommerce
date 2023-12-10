c8e55d3722cb5710c83f9837acacbc6841788efe
Koushik Ahmed <koushikxahmed@gmail.com>
2023-12-10 14:45:21 +0600

    cent to dollar converter added.

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,13 @@ const productionUrl = "https://strapi-store-server.onrender.com/api";
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
+
+// converting to dollar
+
+export const formatPrice = (price) => {
+  const dollarAmount = new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency: "USD",
+  }).format((price / 100).toFixed(2));
+  return dollarAmount;
+};
