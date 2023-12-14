f5248c435ad6bfdff62f2eb61ff20ea559900c00
Koushik Ahmed <koushikxahmed@gmail.com>
2023-12-14 19:38:19 +0600

    filter functionality added.

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,7 +6,8 @@ import FormRange from "./FormRange";
 import FormCheckBox from "./FormCheckBox";
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* search */}
@@ -15,6 +16,7 @@ const Filters = () => {
         label={"search product"}
         name={"search"}
         size={"input-sm"}
+        defaultValue={search}
       />
       {/* CATEGORIES */}
       <FormSelect
@@ -22,6 +24,7 @@ const Filters = () => {
         name={"category"}
         list={meta.categories}
         size={"select-sm"}
+        defaultValue={category}
       />
       {/* COMPANIES */}
       <FormSelect
@@ -29,6 +32,7 @@ const Filters = () => {
         name={"company"}
         list={meta.companies}
         size={"select-sm"}
+        defaultValue={company}
       />
       {/* ORDER */}
       <FormSelect
@@ -36,14 +40,21 @@ const Filters = () => {
         name={"order"}
         list={["a-z", "z-a", "high", "low"]}
         size={"select-sm"}
+        defaultValue={order}
       />
       {/* Price */}
-      <FormRange name={"price"} label={"select price"} size={"range-sm"} />
+      <FormRange
+        name={"price"}
+        label={"select price"}
+        size={"range-sm"}
+        defaultValue={price}
+      />
       {/* shipping */}
       <FormCheckBox
         name={"shipping"}
         label={"free shipping"}
         size={"checkbox-sm"}
+        defaultValue={shipping}
       />
       {/* buttons */}
       <button type="submit" className="btn btn-primary btn-sm">
