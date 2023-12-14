13c3218786a4eebae9b3fa483cdd8b1557788fce
Koushik Ahmed <koushikxahmed@gmail.com>
2023-12-14 17:18:55 +0600

    search | select category | sortby added.

--- src/components/index.js
+++ src/components/index.js
@@ -11,3 +11,4 @@ export { default as Filters } from "./Filters";
 export { default as ProductContainer } from "./ProductContainer";
 export { default as PaginationContainer } from "./PaginationContainer";
 export { default as ProductList } from "./ProductList";
+export { default as FormSelect } from "./FormSelect";
